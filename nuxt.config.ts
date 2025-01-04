af63910963d779b4819ef780c26811b1c4dbdc0c
tsukiyama-3 <tsukiyama.mail@gmail.com>
2025-01-05 01:53:56 +0900

    第5章 公開する

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@nuxt/icon'],
+  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxthub/core'],
   devtools: { enabled: true },
   app: {
     head: {
